9a602598e6e5c801099f37b74609f7179644ffb7
Otterman <leroyalexis7633@gmail.com>
2019-09-03 16:00:29 +0200

    Test intit

---
+++ src/main/java/fr/epsi/Test/AppTest.java
@@ -0,0 +1,17 @@
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class AppTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class).addClass(fr.epsi.App.class).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+}
